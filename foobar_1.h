0cb74b4e1cca26c84e1e3156b079729ac5621ec6
Nathaniel Chen <nc1514@princeton.edu>
2025-12-27 22:39:49 -0500

    reinstate updates

---
+++ foobar_1.h
@@ -0,0 +1,5 @@
+#pragma once 
+#include "./include/k2c_tensor_include.h" 
+void foobar_1(k2c_tensor* keras_tensor_10_input, k2c_tensor* keras_tensor_11_output); 
+void foobar_1_initialize(); 
+void foobar_1_terminate(); 
