cf6811443a406ae45982f6d35e770c17f85601a6
Matthew Waller <hello@cephalopod.studio>
2026-05-04 14:01:08 -0500

    Remove unneeded files.

--- foobar.h
+++
@@ -1,5 +0,0 @@
-#pragma once 
-#include "./include/k2c_tensor_include.h" 
-void foobar(k2c_tensor* keras_tensor_8_input, k2c_tensor* keras_tensor_9_output); 
-void foobar_initialize(); 
-void foobar_terminate(); 
